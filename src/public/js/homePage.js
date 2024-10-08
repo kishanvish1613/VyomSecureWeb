30f071afb737a5fb2fa03507fd4ec5b82d6ac1bf
kishanvish1613 <100762979+kishanvish1613@users.noreply.github.com>
2024-10-08 14:15:45 +0530

    new ui

--- src/public/js/homePage.js
+++ src/public/js/homePage.js
@@ -28,3 +28,41 @@ function faqFunction() {
 
 // Initialize the FAQ functionality
 faqFunction();
+
+
+document.addEventListener('mousemove', function (e) {
+    const circle = document.getElementById('hero-abso-circle-container');
+    const x = e.pageX;
+    const y = e.pageY;
+  
+    // Set the circle's position to the cursor's coordinates
+    circle.style.left = `${x}px`;
+    circle.style.top = `${y}px`;
+});
+  
+
+
+
+
+
+
+const humburger = document.getElementById('humburger');
+const menuCircle = document.querySelector('.menu-circle');
+const menuHidden = document.getElementById('menu-hidden');
+const menuItems = document.querySelectorAll('#menu-hidden ul li');
+
+humburger.addEventListener('click', () => {
+    menuCircle.classList.toggle('active');
+    document.body.classList.toggle('menu-open');
+
+    // When the circle expands, delay the showing of text
+    if (menuCircle.classList.contains('active')) {
+        // Delay to match the circle's expansion time
+        setTimeout(() => {
+            menuItems.forEach(item => item.classList.add('show'));
+        }, 800); // Same as circle's transition time (0.8s)
+    } else {
+        menuItems.forEach(item => item.classList.remove('show'));
+    }
+});
+
